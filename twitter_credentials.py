e9636d2be028efc167cc268bbc7709b3718b8de6
Aastha Zala <57791406+aasthazala@users.noreply.github.com>
2021-08-06 10:01:34 -0400

    Add files via upload
    Here, you have to enter your credential which you can get from Twitter. You have to apply for Twitter Developer option.

---
+++ twitter_credentials.py
@@ -0,0 +1,4 @@
+ACCESS_TOKEN = "Enter your access_token"
+ACCESS_TOKEN_SECRET = "Enter your access_token_secret"
+CONSUMER_KEY = "Enter your consumer_key"
+CONSUMER_SECRET = "Enter your consumer_secret"
